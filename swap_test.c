ea761862c8f6cd60ebca7f589864d4da622b7472
Nguyen The Trung <thetrung@TheTrung.local>
2020-07-22 18:00:36 +0700

    add swap_test

---
+++ swap_test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// Compile Test: 
+// > gcc swap_test.c -o swap_test
+//
+
+void swap(int* A, int* B)
+{
+    int temp = *A; printf("temp = *A   = (%i)\n", temp);
+    *A = *B;       printf("*A   = *B   = (%i) at %p\n", *A, A);
+    *B = temp;     printf("*B   = temp = (%i) at %p\n", *B, B);
+}
+
+int main(void)
+{
+    // Init
+    int a = 5;
+    int b = 10;
+
+    printf("---------------------------------------\n");
+    printf("Before swapping: (%i) - (%i) \n", a, b);
+
+    printf("A = %p\n", &a);
+    printf("B = %p\n", &b);
+
+    printf("---------------------------------------\n");
+
+    swap(&a, &b); // out -> a & b
+
+    printf("---------------------------------------\n");
+
+    printf("After swapping: (%i) - (%i) \n", a, b);
+
+    printf("A = %p\n", &a);
+    printf("B = %p\n", &b);
+
+    printf("---------------------------------------\n");
+
+   
+    // That's all about storing multiple enum in a single value.
+    // @thetrung 2020
+    //
+}
